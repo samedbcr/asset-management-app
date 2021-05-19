27ed1796bea0622e320e050e81ac69f213825cce
dogukanaydn <ddogukanaydin@gmail.com>
2021-05-19 22:31:09 +0300

    Added login screen

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'Asset Management',
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
