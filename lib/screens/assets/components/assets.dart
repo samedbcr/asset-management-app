c12f57eb81980fbebd1185bb1ae6f65cc159748a
samedbcr <bicerr252@gmail.com>
2021-06-02 22:59:34 +0300

    fixed googleservices.json issue. added assets get service and connected into view

--- lib/screens/assets/components/assets.dart
+++ lib/screens/assets/components/assets.dart
@@ -1,11 +1,16 @@
+import 'package:admin/controllers/AssetsController.dart';
 import 'package:admin/theme/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import '../../../extensions/string_extension.dart';
 
 import '../../../constants.dart';
 import '../../../responsive.dart';
 
 class Assets extends StatelessWidget {
-  const Assets({
+  final AssetsController _controller = AssetsController();
+
+  Assets({
     Key key,
   }) : super(key: key);
 
@@ -26,86 +31,127 @@ class Assets extends StatelessWidget {
               scrollDirection: (Responsive.isMobile(context))
                   ? Axis.horizontal
                   : Axis.vertical,
-              child: DataTable(
-                horizontalMargin: 0,
-                columnSpacing: defaultPadding,
-                dataRowHeight: 100,
-                columns: [
-                  DataColumn(
-                    label: Text(
-                      "AssetID",
-                      style: TextStyle(color: AppConstants.darkBlueColor),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      "Asset Name",
-                      style: TextStyle(color: AppConstants.darkBlueColor),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      "Product Type",
-                      style: TextStyle(color: AppConstants.darkBlueColor),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      "Quantity",
-                      style: TextStyle(color: AppConstants.darkBlueColor),
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      "Options",
-                      style: TextStyle(color: AppConstants.darkBlueColor),
-                    ),
-                  ),
-                ],
-                rows: List.generate(
-                  10,
-                  (index) => recentFileDataRow(),
-                ),
-              ),
+              child: _callAssetsStream(),
             ),
           ),
         ],
       ),
     );
   }
-}
 
-DataRow recentFileDataRow() {
-  return DataRow(
-    cells: [
-      DataCell(Text("123456")),
-      DataCell(ListTile(
-        title: Text("Macbook Pro"),
-        subtitle: Text(
-          "Macbook Pro 13 inch",
-          style: TextStyle(color: AppConstants.greenColor),
+  Widget _callAssetsStream() {
+    return StreamBuilder(
+      stream: _controller.getAssets(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return Text('Waiting');
+            break;
+          case ConnectionState.active:
+          case ConnectionState.done:
+            if (snapshot.hasData) {
+              return buildDataTable(snapshot.data.docs);
+            } else {
+              return Text("Assets is Empty!");
+            }
+            break;
+          default:
+            return Text('Active');
+        }
+      },
+    );
+  }
+
+  DataTable buildDataTable(List<QueryDocumentSnapshot> assets) {
+    return DataTable(
+      horizontalMargin: 0,
+      columnSpacing: defaultPadding,
+      dataRowHeight: 100,
+      columns: [
+        DataColumn(
+          label: Text(
+            "AssetID",
+            style: TextStyle(color: AppConstants.darkBlueColor),
+          ),
         ),
-      )),
-      DataCell(Text("Laptop")),
-      DataCell(Text("20")),
-      DataCell(
-        Row(
-          children: [
-            IconButton(
-                icon: Icon(
-                  Icons.edit,
-                  color: AppConstants.greenColor,
-                ),
-                onPressed: () {}),
-            IconButton(
-                icon: Icon(
-                  Icons.delete,
-                  color: Colors.red,
-                ),
-                onPressed: () {}),
-          ],
+        DataColumn(
+          label: Text(
+            "Asset Name",
+            style: TextStyle(color: AppConstants.darkBlueColor),
+          ),
         ),
+        DataColumn(
+          label: Text(
+            "Product Type",
+            style: TextStyle(color: AppConstants.darkBlueColor),
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            "Quantity",
+            style: TextStyle(color: AppConstants.darkBlueColor),
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            "Options",
+            style: TextStyle(color: AppConstants.darkBlueColor),
+          ),
+        ),
+      ],
+      rows: recentFileDataRow(assets),
+    );
+  }
+
+  List<DataRow> recentFileDataRow(List<QueryDocumentSnapshot> assets) {
+    return List.generate(
+      assets.length,
+      (index) => DataRow(
+        cells: [
+          DataCell(
+            Text(assets[index].id),
+          ),
+          DataCell(
+            ListTile(
+              title: Text(
+                assets[index]["name"],
+              ),
+              subtitle: Text(
+                assets[index]["description"],
+                style: TextStyle(color: AppConstants.greenColor),
+              ),
+            ),
+          ),
+          DataCell(
+            Text(
+              assets[index]["type"].toString().capitalize(),
+            ),
+          ),
+          DataCell(
+            Text(
+              assets[index]["quantity"].toString(),
+            ),
+          ),
+          DataCell(
+            Row(
+              children: [
+                IconButton(
+                    icon: Icon(
+                      Icons.edit,
+                      color: AppConstants.greenColor,
+                    ),
+                    onPressed: () {}),
+                IconButton(
+                    icon: Icon(
+                      Icons.delete,
+                      color: Colors.red,
+                    ),
+                    onPressed: () {}),
+              ],
+            ),
+          ),
+        ],
       ),
-    ],
-  );
+    );
+  }
 }
