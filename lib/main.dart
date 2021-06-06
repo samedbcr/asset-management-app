4af8da1015844b54a8afa641fb8d9c3490e92a30
dogukanaydn <ddogukanaydin@gmail.com>
2021-06-06 18:05:41 +0300

    remove debugmodebanner

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '',
       theme: ThemeData.dark(),
       home: new RootPage(auth: new Auth()),
